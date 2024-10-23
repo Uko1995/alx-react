b1f171ec8adffead5a64f2224cd66ce9859c2f8d
Uko Uwatt <uwattuko@gmail.com>
2024-10-23 01:47:05 +0100

    task 1

---
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const { title } = require('process');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports ={
+    entry: path.resolve(__dirname, './js/dashboard_main.js'),
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    mode: 'production',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Webstack Output'}),
+    ],
+};
