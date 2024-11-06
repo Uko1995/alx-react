6f3408cae6d9e7fba9c3b1afe2cf2dca7d5771cf
Uko Uwatt <uwattuko@gmail.com>
2024-11-06 15:55:58 +0100

    task 2: added label and inputs for email and password

--- 0x01-react_intro/task_2/dashboard/src/App.js
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -5,21 +5,21 @@ import { getFooterCopy, getFullYear } from './utils';
 function App() {
   return (
     <>
-    <header className='App-header'>
+    <div className='App-header'>
       <img src={logo} alt='holberton-logo' />
       <h1>School dashboard</h1>
-    </header>
-    <body className='App-body'>
+    </div>
+    <div className='App-body'>
       <p>Login to access the full dashboard</p>
       <label htmlFor='email'>Email</label>
       <input type='email' name='email' id='email' />
       <label htmlFor='password'>Password</label>
       <input type='password' name='password' id='password' />
       <button>OK</button>
-    </body>
-    <footer className='App-footer'>
+    </div>
+    <div className='App-footer'>
       <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
-    </footer>
+    </div>
     </>
   );
 }
