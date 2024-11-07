50ef0774b8ca227006b33c96a1092d99721c0a2a
Uko Uwatt <uwattuko@gmail.com>
2024-11-07 04:13:17 +0100

    task 6: installed and configured enzyme

--- 0x01-react_intro/task_3/dashboard/src/setupTests.js
+++ 0x01-react_intro/task_3/dashboard/src/setupTests.js
@@ -3,3 +3,7 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import enzyme from 'enzyme';
+import Adapter from '@cfaester/enzyme-adapter-react-18';
+
+enzyme.confugure({ adapter: new Adapter() });
