5ceffc3ae392b7475df01a0f6f3a53b648d51558
Uko Uwatt <uwattuko@gmail.com>
2025-01-03 19:03:15 +0100

    added eventlisteners for the key presses

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -8,38 +8,66 @@ import PropTypes from 'prop-types';
 import CourseList from '../CourseList/CourseList';
 import { getLatestNotification } from '../utils/utils';
 
-const listCourses = [
-  {id: 1, name: 'ES6', credit: 60},
-  {id: 2, name: 'Webpack', credit: 20},
-  {id: 3, name: 'React', credit: 40}
-];
 
-const listNotifications = [
-  {id: 1, type: 'default', value: 'New course available'},
-  {id: 2, type: 'urgent', value: 'New resume available'},
-  {id: 3, type: 'urgent', html: { __html: getLatestNotification() }}
-];
 
 
 class App extends React.Component {
-  constructor(isLoggedIn) {
-    super(isLoggedIn);
-    this.isLoggedIn = isLoggedIn;
+  constructor(props) {
+    super(props);
+    this.handleKey = this.handleKey.bind(this);
+  };
+
+  listCourses = [
+    {id: 1, name: 'ES6', credit: 60},
+    {id: 2, name: 'Webpack', credit: 20},
+    {id: 3, name: 'React', credit: 40}
+  ];
+  
+  listNotifications = [
+    {id: 1, type: 'default', value: 'New course available'},
+    {id: 2, type: 'urgent', value: 'New resume available'},
+    {id: 3, type: 'urgent', html: { __html: getLatestNotification() }}
+  ];
+
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKey);
   }
 
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKey);
+  }
+
+  handleKey(event) {
+    if (event.ctrlKey && event.key === 'h') {
+      alert('Logging you out');
+      this.props.logOut();
+    }
+  };
+
   render() {
+    const { isLoggedIn } = this.props;
     return (
       <>
-        <Notifications listNotifications={listNotifications} displayDrawer={true}/>
+        <Notifications listNotifications={this.listNotifications} displayDrawer={true}/>
         <div className='App'>
           <Header />
-          {this.isLoggedIn ? <CourseList listCourses={listCourses}/> : <Login/>}
+          {isLoggedIn ? <CourseList listCourses={this.listCourses}/> : <Login/>}
           <Footer />
         </div>
       </>
     );  
   }
 }
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+  logOut: PropTypes.func
+};
+
+App.defaultProps = {
+  isLoggedIn: false,
+  logOut: () => {}
+};
 
 /*function App({isLoggedIn=true}) {
   return (
