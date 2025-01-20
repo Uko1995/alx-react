6c8d76917e523def751bb68f83300f2524fd816d
Uko Uwatt <uwattuko@gmail.com>
2025-01-20 07:14:05 +0100

    task 0

---
+++ 0x05-react_state/task_0/dashboard/src/CourseList/CourseListRow.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { StyleSheet, css } from 'aphrodite';
+
+const styles = StyleSheet.create({
+    headerStyle: {
+        backgroundColor: '#deb5b545'
+    },
+    rowStyle: {
+        backgroundColor: '#f5f5f5ab'
+    },
+    th: {
+        border: '1px solid lightgray',
+        padding: '0.5em',
+        fontSize: '1.5em'
+    }
+});
+
+
+
+export default function CourseListRow({isHeader, textFirstCell, textSecondCell}) {
+    if (isHeader === true) {
+        return (
+            <tr className={css(styles.headerStyle)}> 
+                {textSecondCell === null ? (<th className={css(styles.th)} colSpan="2">{textFirstCell}</th>) : (
+                <>
+                    <th className={css(styles.th)} >{textFirstCell}</th>
+                    <th className={css(styles.th)} >{textSecondCell}</th>
+                </>)}
+            </tr>
+        );
+     } else {
+        return (
+            <tr className={css(styles.rowStyle)}>
+                <>
+                    <td className={css(styles.th)}>{textFirstCell}</td>
+                    <td className={css(styles.th)}>{textSecondCell}</td>
+                </>
+            </tr>
+        );
+     }
+};
+
+CourseListRow.propTypes = {
+    isHeader: PropTypes.bool,
+    textFirstCell: PropTypes.string.isRequired,
+    textSecondCell: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+};
---
+++ 0x05-react_state/task_0/dashboard/src/Footer/Footer.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { getFooterCopy, getFullYear } from '../utils/utils';
+import { StyleSheet, css } from 'aphrodite';
+
+const styles = StyleSheet.create({
+    footer: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        borderTop: '2px solid #e01d3f',
+        width: '100%',
+        justifyContent: 'center',
+        position: 'absolute',
+        fontFamily: 'Georgia, Times New Roman, Times, serif',
+    },
+    p: {
+        fontSize: '2.5em',
+        fontStyle: 'italic'
+    }
+});
+
+
+export default function Footer() {
+    return (
+        <>
+            <div className={css(styles.footer)}>
+                <p className={css(styles.p)}>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+            </div>
+        </>
+    )
+}
