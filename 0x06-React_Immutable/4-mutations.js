7954fc5e68949a937c27a8a83335a9f34d22177d
Uko Uwatt <uwattuko@gmail.com>
2025-02-18 06:33:00 +0100

    task 3: mutations

---
+++ 0x06-React_Immutable/4-mutations.js
@@ -0,0 +1,17 @@
+import { Map } from 'immutable';
+
+const map = Map({
+    1: 'Liam',
+    2: 'Noah',
+    3: 'Elijah',
+    4: 'Oliver',
+    5: 'Jacob',
+    6: 'Lucas',
+});
+
+const map2 = Map({
+    2: 'Benjamin',
+    4: 'Oliver'
+});
+
+map.merge(map2);
