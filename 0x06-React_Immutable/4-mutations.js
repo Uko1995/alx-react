55674a27d868f7a147c7ff5b3c8d2a8e998d633d
Uko Uwatt <uwattuko@gmail.com>
2025-02-18 06:50:23 +0100

    task 3: mutations

--- 0x06-React_Immutable/4-mutations.js
+++ 0x06-React_Immutable/4-mutations.js
@@ -1,6 +1,6 @@
 import { Map } from 'immutable';
 
-const map = Map({
+export const map = Map({
     1: 'Liam',
     2: 'Noah',
     3: 'Elijah',
@@ -9,6 +9,9 @@ const map = Map({
     6: 'Lucas',
 });
 
-const map2 = map.set(2, 'Benjamin').set(4, 'Oliver');
+export const map2 = map.set(2, 'Benjamin').set(4, 'Oliver');
 
-export default [map, map2];
+console.log('Before mutation');
+console.log(map.toObject());
+console.log('After mutation');
+console.log(map2.toObject());
