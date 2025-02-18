9e715308c7d5d57fedba4bd8f812f3021c424503
Uko Uwatt <uwattuko@gmail.com>
2025-02-18 07:20:51 +0100

    task 6: deeply merge

--- 0x06-React_Immutable/6-deeply.js
+++ 0x06-React_Immutable/6-deeply.js
@@ -1,7 +1,7 @@
-import { Map, List } from 'immutable';
+import { Map } from 'immutable';
 
 export default function mergeDeeplyElements(page1=Object, page2=Object) {
-  return List(Map(page1).mergeDeep(Map(page2)));
+  return Map(page1).mergeDeep(Map(page2));
 }
 
 const page1 = {
